3d993f9fb3ef7b608acbf1c6494d2fde6cab4af4
AgusDOLARD <agust_22_10@hotmail.com>
2024-01-05 23:01:21 -0300

    multiline support

--- lua/backout/utils.lua
+++ lua/backout/utils.lua
@@ -2,22 +2,16 @@ local M = {}
 local config = require("backout.config")
 local logger = require("backout.logger")
 
----@param position number
+---Get the character from a given row and column
+---@param row number
+---@param col number
 ---@return string
-local function getCharacterPositionOnLine(position)
-    local lineString = vim.api.nvim_get_current_line()
-    if position <= #lineString then
-        local char = lineString:sub(position, position)
-        return char
-    elseif string.len(lineString) == 0 then
-        logger.debug("Empty line")
-        return ""
-    else
-        logger.debug("Cannot find any character on line: " .. lineString)
-        return ""
-    end
+local function getChar(row, col)
+    local line = vim.fn.getline(row)
+    return line:sub(col, col)
 end
 
+---Returns true if the given character is defined in opts.chars
 ---@param char string
 ---@return boolean
 local function isKnownCharacter(char)
@@ -28,42 +22,42 @@ local function isKnownCharacter(char)
     return false
 end
 
----@param forward boolean
----@return number
-local function findChar(forward)
-    local col = vim.fn.col
+---Recursively search for the closest character
+---@param forward boolean if true means going forward else going backwards
+---@param row number initial row number
+---@param col number initial column number
+---@return number,number # return row, column of found character else returns -1,-1
+local function findChar(forward, row, col)
+    if row < 1 or row > vim.fn.line("$") then return -1, -1 end
+
     local step = forward and 1 or -1
-    local position = forward and col(".") or col(".") + step
+    local function colLength(f) return f and vim.fn.col("$") or 1 end
 
-    local found = false
-    repeat
-        local char = getCharacterPositionOnLine(position)
-        found = isKnownCharacter(char)
-        if not found then position = position + step end
-    until found or char == ""
-    if found then
-        logger.debug("char found on col:" .. position)
-        return position
-    else
-        return -1
+    for i = col, colLength(forward), step do
+        local char = getChar(row, i)
+        if isKnownCharacter(char) then
+            logger.debug("char: " .. char .. " found on " .. row .. ":" .. i)
+            return row, i
+        end
     end
+
+    return findChar(forward, row + step, colLength(not forward))
 end
 
----@param column number
-local function moveCursor(column)
-    local row = vim.fn.line(".")
-    logger.debug("Moving cursor to " .. row .. ":" .. column)
-    vim.api.nvim_win_set_cursor(0, { row, column })
-end
+---@param row number
+---@param col number
+local function moveCursor(row, col) vim.api.nvim_win_set_cursor(0, { row, col }) end
 
 M.back = function()
-    local pos = findChar(false) - 1
-    if pos >= 0 then moveCursor(pos) end
+    local cursor = vim.fn.getcurpos(0)
+    local row, col = findChar(false, cursor[2], cursor[3] - 1)
+    if row ~= -1 then moveCursor(row, col - 1) end
 end
 
 M.out = function()
-    local pos = findChar(true)
-    if pos >= 0 then moveCursor(pos) end
+    local cursor = vim.fn.getcurpos(0)
+    local row, col = findChar(true, cursor[2], cursor[3])
+    if row ~= -1 then moveCursor(row, col) end
 end
 
 return M
