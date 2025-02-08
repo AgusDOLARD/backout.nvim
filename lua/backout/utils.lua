7caa6a0c2c08f411548707f9dfd4a3ce5b0d4197
user <user@debian.local>
2025-02-08 19:11:26 -0300

    fix: multiline moving

--- lua/backout/utils.lua
+++ lua/backout/utils.lua
@@ -5,9 +5,9 @@ local logger = require("backout.logger")
 ---Get the character from a given row and column
 ---@param row number
 ---@param col number
----@return string
+---@return string|nil
 local function getChar(row, col)
-    if row < 0 or col < 0 then return "invalid" end
+    if row < 1 or col < 1 then return nil end
     local line = vim.fn.getline(row)
     return line:sub(col, col)
 end
@@ -23,36 +23,44 @@ local function isKnownCharacter(char)
     return false
 end
 
----Recursively search for the closest character
+---Iteratively search for the closest character
 ---@param forward boolean if true means going forward else going backwards
 ---@param row number initial row number
 ---@param col number initial column number
 ---@return number,number # return row, column of found character else returns -1,-1
 local function findChar(forward, row, col)
-    if row < 1 or row > vim.fn.line("$") then return -1, -1 end
-
     local step = forward and 1 or -1
-    local function colLength(f) return f and vim.fn.col("$") or 1 end
+    local max_row = vim.fn.line("$")
 
-    for i = col, colLength(forward), step do
-        local char = getChar(row, i)
-        if isKnownCharacter(char) then
-            logger.debug("char: " .. char .. " found on " .. row .. ":" .. i)
-            return row, i
+    while row >= 1 and row <= max_row do
+        local max_col = vim.fn.col({ row, "$" }) - 1
+        local min_col = 1
+
+        for i = col, forward and max_col or min_col, step do
+            local char = getChar(row, i)
+            if char and isKnownCharacter(char) then
+                logger.debug("char: " .. tostring(char) .. " found on " .. row .. ":" .. i)
+                return row, i
+            end
         end
-    end
 
-    return findChar(forward, row + step, colLength(not forward))
+        row = row + step
+        col = forward and min_col or max_col
+    end
+    return -1, -1
 end
 
 ---@param row number
 ---@param col number
-local function moveCursor(row, col) vim.api.nvim_win_set_cursor(0, { row, col }) end
+local function moveCursor(row, col)
+    vim.api.nvim_win_set_cursor(0, { row, col })
+end
+
 
 M.back = function()
     local cursor = vim.fn.getcurpos(0)
-    local row, col = findChar(false, cursor[2], cursor[3] - 1)
-    if row ~= -1 then moveCursor(row, col - 1) end
+    local row, col = findChar(false, cursor[2], cursor[3])
+    if row ~= -1 then moveCursor(row, col) end
 end
 
 M.out = function()
