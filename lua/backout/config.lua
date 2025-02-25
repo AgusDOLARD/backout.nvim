baa5b08de99fc74e53a802ce3cb7d3a5b3bb1c80
AgusDOLARD <agust_22_10@hotmail.com>
2025-02-25 16:46:13 -0300

    sanitize opts.chars

--- lua/backout/config.lua
+++ lua/backout/config.lua
@@ -7,16 +7,21 @@ local defaults = {
 
 M.opts = {}
 
-M.charsTable = function()
-    local c = {}
-    for letter in M.opts.chars:gmatch(".") do
-        table.insert(c, letter)
-    end
-    return c
+--- Parse the chars from config to work with special characters
+--- @param chars string
+--- @return string :sanatized string
+local parseChars = function(chars)
+    local sanitized = string.gsub(
+        chars,
+        "[%[%]]",
+        function(c) return "%" .. c end
+    )
+    return string.format("[%s]", sanitized)
 end
 
 M.setup = function(opts)
     M.opts = vim.tbl_deep_extend("force", {}, defaults, opts or {})
+    M.opts.chars = parseChars(M.opts.chars)
 end
 
 return M
